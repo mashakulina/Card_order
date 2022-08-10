63bbf550b9fd924792b127d51a508721be2660d4
mashakulina <masha_kulina@mail.ru>
2022-08-10 12:02:26 +0300

    Исправленный тест

--- src/test/java/ru/netology/TestCardForm.java
+++ src/test/java/ru/netology/TestCardForm.java
@@ -11,13 +11,13 @@ public class TestCardForm {
 
     @Test
     void shouldTestSomething() {
-        open("http://localhost:7777");
-        SelenideElement form = $("#root > div > form");
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
         form.$("[data-test-id=name] input").setValue("Иван Иванов");
         form.$("[data-test-id=phone] input").setValue("+79672699504");
         form.$("[data-test-id=agreement]").click();
-        form.$("#root > div > form > div:nth-child(4) > button").click();
-        $("#root > div > div > p").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        form.$("button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
 
     }
 }
